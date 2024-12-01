3cef68b763255bba2056f2d53859ecc62ad5b69d
Dopitek98 <dopitek@wp.pl>
2024-12-01 18:52:00 +0100

    missing file

--- src/test/java/com/dopitek/tests/vendorportal/model/VendorPortalTestData.java
+++ src/test/java/com/dopitek/tests/vendorportal/model/VendorPortalTestData.java
@@ -1,2 +1,12 @@
-package com.dopitek.tests.vendorportal.model;public record VendorPortalTestData() {
+package com.dopitek.tests.vendorportal.model;
+
+public record VendorPortalTestData(String username,
+                                   String password,
+                                   String monthlyEarning,
+                                   String annualEarning,
+                                   String profitMargin,
+                                   String availableInventory,
+                                   String searchKeyword,
+                                   int searchResultsCount) {
+
 }
